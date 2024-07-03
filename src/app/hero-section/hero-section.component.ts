baa514fc2720fb1c6d1c08372338ea3663fa55e9
Aadithya Sai <aadithyas0610@gmail.com>
2024-07-03 21:50:51 +0530

    Better scroll anims

--- src/app/hero-section/hero-section.component.ts
+++ src/app/hero-section/hero-section.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
+import { gsap } from '../utils/gsap';
 
 @Component({
   selector: 'portfolio-hero-section',
   standalone: true,
   imports: [],
-  templateUrl: './hero-section.component.html'
+
+  templateUrl: './hero-section.component.html',
 })
 export class HeroSectionComponent {
+  constructor(protected element: ElementRef) {
+    this.element = element;
+  }
 
+  heroContent = viewChild('heroContent', { read: ElementRef });
+
+  ngAfterViewInit() {
+    const heroContent = this.heroContent()!;
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: heroContent.nativeElement,
+        pin: this.element.nativeElement,
+        scrub: 1,
+        start: 'top top',
+        end: '+=1000',
+      },
+    });
+
+    tl.to(
+      heroContent.nativeElement,
+      {
+        opacity: 0,
+      },
+      0.8
+    );
+  }
 }
--- src/app/project-section/project-section.component.ts
+++ src/app/project-section/project-section.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChildren } from '@angular/core';
 import { PanelComponent } from '../panel/panel.component';
+import { gsap } from '../utils/gsap';
+import { ProjectCardComponent } from '../project-card/project-card.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'portfolio-project-section',
   standalone: true,
-  imports: [PanelComponent],
+  imports: [PanelComponent, ProjectCardComponent, NgFor],
   templateUrl: './project-section.component.html',
 })
-export class ProjectSectionComponent {}
+export class ProjectSectionComponent {
+  projects = [
+    {
+      title: 'Project 1',
+      description: 'This is a description of project 1.',
+      image: 'https://via.placeholder.com/150',
+    },
+    {
+      title: 'Project 2',
+      description: 'This is a description of project 2.',
+      image: 'https://via.placeholder.com/150',
+    },
+    {
+      title: 'Project 3',
+      description: 'This is a description of project 3.',
+      image: 'https://via.placeholder.com/150',
+    },
+  ];
+
+  panels = viewChildren(PanelComponent, { read: ElementRef });
+
+  constructor(protected element: ElementRef) {
+    this.element = element;
+  }
+
+  ngAfterViewInit() {
+    const leftPanel = this.panels()[0];
+    const rightPanel = this.panels()[1];
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: this.element.nativeElement,
+        start: 'top top',
+        end: () => 'bottom 80%',
+        pin: leftPanel.nativeElement,
+        pinSpacing: false,
+        markers: true,
+        // scrub: 1,
+      },
+    });
+  }
+}
