686f3015d24dbd695eaada12472637660d596b13
Aadithya Sai <aadithyas0610@gmail.com>
2024-07-04 21:45:28 +0530

    added new sections to landing page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { HeroSectionComponent } from '../hero-section/hero-section.component';
 import { ProjectSectionComponent } from '../project-section/project-section.component';
 import { HeaderComponent } from '../header/header.component';
+import { AboutSectionComponent } from '../about-section/about-section.component';
+import { ContactSectionComponent } from '../contact-section/contact-section.component';
 
 @Component({
   selector: 'portfolio-landing-page',
   standalone: true,
-  imports: [HeroSectionComponent, ProjectSectionComponent, HeaderComponent],
+  imports: [
+    HeroSectionComponent,
+    ProjectSectionComponent,
+    HeaderComponent,
+    AboutSectionComponent,
+    ContactSectionComponent,
+  ],
   templateUrl: './landing-page.component.html',
 })
 export class LandingPageComponent {}
