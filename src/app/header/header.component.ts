3eed16f7db88c945ac2426000c573a0a439c74cb
Aadithya Sai <aadithyas0610@gmail.com>
2024-07-04 21:43:35 +0530

    Added anims

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChildren } from '@angular/core';
+import { gsap, ScrollTrigger } from '../utils/gsap';
 
 @Component({
   selector: 'portfolio-header',
   standalone: true,
   imports: [],
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  constructor(protected element: ElementRef) {
+    this.element = element;
+  }
 
+  items = viewChildren('item', { read: ElementRef });
+
+  ngAfterViewInit() {
+    const items = this.items().map((item) => item.nativeElement);
+    const loadTl = gsap.timeline({
+      delay: 0.5,
+      defaults: {
+        duration: 1,
+      },
+    });
+
+    loadTl.from(items.reverse(), {
+      y: 25,
+      opacity: 0,
+      stagger: 0.2,
+    });
+
+    const scrollTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: this.element.nativeElement,
+        start: 'top -1000',
+        end: '+=1000',
+        toggleActions: 'play none reverse reverse',
+      },
+    });
+
+    scrollTl.to(this.element.nativeElement, {
+      opacity: 0,
+      y: -100,
+    });
+  }
 }
