38dcc1353be488080844c69b0122acb42437a30b
Aadithya Sai <aadithyas0610@gmail.com>
2024-07-04 21:44:48 +0530

    dummy contacts section

---
+++ src/app/contact-section/contact-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-contact-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-section.component.html'
+})
+export class ContactSectionComponent {
+
+}
