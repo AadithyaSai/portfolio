0a2d5e2ce6076e3be08e02484c2e55649eb89787
Aadithya Sai <aadithyas0610@gmail.com>
2024-07-05 20:16:11 +0530

    Revamped about

--- src/app/about-section/about-section.component.ts
+++ src/app/about-section/about-section.component.ts
@@ -1,11 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, viewChildren } from '@angular/core';
+import { gsap } from '../utils/gsap';
 
 @Component({
   selector: 'portfolio-about-section',
   standalone: true,
   imports: [],
-  templateUrl: './about-section.component.html'
+  templateUrl: './about-section.component.html',
 })
 export class AboutSectionComponent {
+  constructor(protected element: ElementRef) {
+    this.element = element;
+  }
 
+  headings = viewChildren('heading', { read: ElementRef });
+  contents = viewChildren('content', { read: ElementRef });
+  image = viewChild('image', { read: ElementRef });
+
+  ngAfterViewInit() {
+    const headings = this.headings().map((heading) => heading.nativeElement);
+    const contents = this.contents().map((content) => content.nativeElement);
+    const image = this.image()?.nativeElement;
+
+    gsap.to(this.element.nativeElement, {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: this.element.nativeElement,
+        start: 'bottom 50%',
+        toggleActions: 'play none reverse none',
+        // markers: true,
+      },
+    });
+
+    const scrollTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: this.element.nativeElement,
+        start: 'top 60%',
+        end: '+=1000',
+        once: true,
+        // markers: true,
+      },
+      defaults: {
+        duration: 1,
+      },
+    });
+
+    scrollTl.from(image, {
+      y: 50,
+      opacity: 0,
+    });
+    scrollTl.from(
+      headings,
+      {
+        x: -50,
+        opacity: 0,
+        stagger: 0.3,
+        ease: 'back.out(2.3)',
+      },
+      0.5
+    );
+    scrollTl.from(
+      contents,
+      {
+        y: 50,
+        opacity: 0,
+        stagger: 0.3,
+      },
+      '<0.4'
+    );
+  }
 }
